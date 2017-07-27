647a2a1a29063a566e086e070c1d7465565b2a8f
benjhill78 <benjhill78@gmail.com>
2017-07-27 16:01:45 -0400

    ReadMe

--- README.md
+++ README.md
@@ -7,3 +7,28 @@ Once the customer decides what the want to purchase they are asked how many of e
 If there are enough of that item it will allow a purchase if not it will tell the customer there is not enough in stock.
 
 Once the purchase is made the database will be updated with the item(s) removed from the stock and the customer can purchase other items.
+
+
+To get the customers to be able to purchase we must first create a MySQL Database using the file bamazon_db.sql.
+
+<a href="dbcreate.jpg"><img src="dbcreate.jpg" alt="dbcreation" height="40" width="40" /></a>
+
+
+Then create a bamazonCustomer.js file for the customer code and connect it to the DB and show the connected id along with the products.
+
+<a href="dbconnect.jpg"><img src="dbconnect.jpg" alt="dbconnection" height="40" width="40" /></a>
+
+
+Show the Customer the products and asks them to select the ID.  If there is no id with that number it will ask them to select a correct item_id
+
+<a href="idselect.jpg"><img src="idselect.jpg" alt="idselection" height="40" width="40" /></a>
+
+
+Ask the user how many they want. If they choose the number higher than the remaining stock throw an error.
+
+<a href="amountselect.jpg"><img src="amountselect.jpg" alt="amountselection" height="40" width="40" /></a>
+
+
+The full screen showing selections and price.
+
+<a href="finished.jpg"><img src="finished.jpg" alt="finished" height="40" width="40" /></a>
--- bamazon_db.sql
+++ bamazon_db.sql
@@ -1,45 +1,45 @@
--- create database bamazon_db --
+create database bamazon_db
 
--- use bamazon_db --
+use bamazon_db
 
--- create table Products( 
--- item_id integer(30) auto_increment not null,
--- product_name varchar (200) not null,
--- department_name varchar (200) not null,
--- price integer (100) not null,
--- stock_quantity integer (100) not null,
--- primary key(item_id)
--- );
+create table Products( 
+item_id integer(30) auto_increment not null,
+product_name varchar (200) not null,
+department_name varchar (200) not null,
+price integer (100) not null,
+stock_quantity integer (100) not null,
+primary key(item_id)
+);
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Wireless Mouse", "Electronics", 20, 10)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Wireless Mouse", "Electronics", 20, 10)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Wireless Keyboard", "Electronics", 25.00, 13)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Wireless Keyboard", "Electronics", 25.00, 13)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Headphones", "Electronics", 50.00, 22)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Headphones", "Electronics", 50.00, 22)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Printer Paper", "Office Supplies", 12.00, 30)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Printer Paper", "Office Supplies", 12.00, 30)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Chair", "Office Furniture", 199.00, 1)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Chair", "Office Furniture", 199.00, 1)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Desk", "Office Furniture", 300.00, 5)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Desk", "Office Furniture", 300.00, 5)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Lamp", "Lighting", 26.00, 6)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Lamp", "Lighting", 26.00, 6)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Backpack", "School Supplies", 32.00, 14)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Backpack", "School Supplies", 32.00, 14)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Envelopes", "Office Supplies", 8.00, 35)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Envelopes", "Office Supplies", 8.00, 35)
 
--- insert into Products (product_name, department_name, price, stock_quantity)
--- values ("Pen", "Office Supplies", 2.00, 50)
+insert into Products (product_name, department_name, price, stock_quantity)
+values ("Pen", "Office Supplies", 2.00, 50)
 
 
 
